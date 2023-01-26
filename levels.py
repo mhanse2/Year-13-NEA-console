7b12523f29c95260b16fe0de1a3281276b86da3d
Maxwell Hansen <113341427+mhanse2@users.noreply.github.com>
2023-01-26 15:52:32 +0000

    added value stuff

---
+++ levels.py
@@ -0,0 +1,45 @@
+# the highest level the player can achieve
+CAP = 12
+
+# question order
+order = [
+    'Addition',
+    'Subtraction',
+    'NegativeSubtraction',
+    'Multiplication',
+    'Division',
+    'Power',
+    'Root',
+    'SquareArea',
+    'SquarePerimeter',
+    'Triangle',
+    'CirclePerimeter',
+    'CircleArea'
+]
+
+# used for Addition, Subtraction & NegativeSubtraction
+add_values = [
+    [1, 2, 3, 4, 5],
+    [2, 4, 5, 6, 8, 10],
+    [5, 7, 11, 15, 18],
+    [10, 11, 13, 15, 20],
+    [13, 15, 19, 21, 25],
+    [19, 23, 25, 27, 30],
+    [24, 27, 30, 35, 37],
+    [30, 35, 36, 38, 40],
+    [36, 40, 42, 45, 47],
+    [45, 48, 50, 52, 55],
+    [50, 53, 55, 57, 60],
+    [55, 58, 60, 63, 65],
+    [60, 63, 66, 69, 70]
+]
+
+# used for Multiplication, Division, Triangle, 
+# SquareArea, SquarePerimeter and CirclePerimeter
+mult_values = [
+    [2, 4],
+    [2, 4, 3, 5, 6],
+    [4, 5, 6, 9, 10],
+    [4, 6, 7, 10, 11],
+    [5, 7, 11, 15, 20]
+]
